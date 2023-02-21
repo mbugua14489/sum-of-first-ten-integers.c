aa462b45275b1cf8404d7bc57e4b5c7a1a36ef14
mbugua14489 <122589720+mbugua14489@users.noreply.github.com>
2023-02-21 23:58:03 +0300

    Add files via upload

---
+++ sum of first 10 intergers.c
@@ -0,0 +1,12 @@
+//sum of the first ten natural numbers
+#include<stdio.h>
+int main()
+{
+    int n;
+int sum=1+2+3+4+5+6+7+8+9+10;
+for(n>=0; n<=10; n++);{
+    printf("%d\n",n);
+}
+printf("the sum is%d\n", sum);
+    return 0;
+}
